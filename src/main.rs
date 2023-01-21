a691570d25010829133a037061ace4ff0b926bea
popotanu <scrapmacaron@gmail.com>
2023-01-21 12:42:55 +0900

    refactor: split some codes into files

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,22 @@
-use rltk::{render_draw_buffer, GameState, Rltk, VirtualKeyCode, RGB};
+use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
-use specs_derive::Component;
-use std::{
-    arch::x86_64::_SIDD_LEAST_SIGNIFICANT,
-    cmp::{max, min},
-};
 
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
+mod components;
+pub use components::*;
+mod map;
+pub use map::*;
+mod player;
+pub use player::*;
 
-#[derive(Component)]
-struct Renderable {
-    glyph: rltk::FontCharType, // @さんとか
-    fg: RGB,                   // foreground
-    bg: RGB,                   // background
-}
-struct State {
+pub struct State {
     ecs: World,
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        // システムにより何らかの変更がqueueに入れられたら,即座に世界に適用する
+        self.ecs.maintain();
+    }
 }
 
 impl GameState for State {
@@ -41,121 +38,6 @@ impl GameState for State {
     }
 }
 
-#[derive(Component, Debug)]
-struct Player {}
-
-#[derive(PartialEq, Copy, Clone)]
-enum TileType {
-    Wall,
-    Floor,
-}
-
-pub fn xy_idx(x: i32, y: i32) -> usize {
-    (y as usize * 80) + x as usize
-}
-
-fn new_map() -> Vec<TileType> {
-    let mut map = vec![TileType::Floor; 80 * 50];
-
-    // 壁との境界をつくる
-    for x in 0..80 {
-        map[xy_idx(x, 0)] = TileType::Wall;
-        map[xy_idx(x, 49)] = TileType::Wall;
-    }
-    for y in 0..50 {
-        map[xy_idx(0, y)] = TileType::Wall;
-        map[xy_idx(79, y)] = TileType::Wall;
-    }
-
-    let mut rng = rltk::RandomNumberGenerator::new();
-
-    // ランダムに壁を作る
-    for _i in 0..400 {
-        let x = rng.roll_dice(1, 79);
-        let y = rng.roll_dice(1, 49);
-        let idx = xy_idx(x, y);
-
-        // mapの中央は壁にしたくない.ここから始めたい
-        if idx != xy_idx(40, 25) {
-            map[idx] = TileType::Wall;
-        }
-    }
-
-    map
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != TileType::Wall {
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(49, max(0, pos.y + delta_y));
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    // player movement
-    match ctx.key {
-        // ctx.keyがなにもないとき(何も押されてないとき)にNoneにマッチする
-        // このとき,何も返さない.何も起こらない
-        None => {}
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {} // default
-        },
-    }
-}
-
-// TileTypeのスライスをw足す
-fn draw_map(map: &[TileType], ctx: &mut Rltk) {
-    let mut y = 0;
-    let mut x = 0;
-    for tile in map.iter() {
-        // タイルの種類に応じてタイルを描く
-        match tile {
-            TileType::Floor => {
-                ctx.set(
-                    x,
-                    y,
-                    RGB::from_f32(0.5, 0.5, 0.5),
-                    RGB::from_f32(0., 0., 0.),
-                    rltk::to_cp437('.'),
-                );
-            }
-            TileType::Wall => {
-                ctx.set(
-                    x,
-                    y,
-                    RGB::from_f32(0.0, 0.8, 0.0),
-                    RGB::from_f32(0., 0., 0.),
-                    rltk::to_cp437('#'),
-                );
-            }
-        }
-
-        // 座標を動かす
-        x += 1;
-        if x > 79 {
-            x = 0;
-            y += 1;
-        }
-    }
-}
-
-impl State {
-    fn run_systems(&mut self) {
-        // システムにより何らかの変更がqueueに入れられたら,即座に世界に適用する
-        self.ecs.maintain();
-    }
-}
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
