47e8dce0390b3a68d8450ceb8b1b84ec87c4f421
popotanu <scrapmacaron@gmail.com>
2023-01-21 18:04:17 +0900

    refactor: `struct Map`を定義してmapを生成するロジックをその中に移す

--- src/map.rs
+++ src/map.rs
@@ -1,8 +1,5 @@
-use crate::rect;
-
 use super::Rect;
 use rltk::{RandomNumberGenerator, Rltk, RGB};
-use specs::rayon::vec;
 use std::cmp::{max, min};
 
 #[derive(PartialEq, Copy, Clone)]
@@ -10,120 +7,104 @@ pub enum TileType {
     Wall,
     Floor,
 }
-pub fn xy_idx(x: i32, y: i32) -> usize {
-    (y as usize * 80) + x as usize
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+    pub rooms: Vec<Rect>,
+    pub width: i32,
+    pub height: i32,
 }
 
-// rustで ///を使うと関数コメントになる.IDEのサポートを受けられる
-/// ランダムに配置された400この壁を持つmapを生成する
-/// 見栄えが悪い.良いmapだとはいえない
-pub fn new_map_test() -> Vec<TileType> {
-    let mut map = vec![TileType::Floor; 80 * 50];
-
-    // 壁との境界をつくる
-    for x in 0..80 {
-        map[xy_idx(x, 0)] = TileType::Wall;
-        map[xy_idx(x, 49)] = TileType::Wall;
-    }
-    for y in 0..50 {
-        map[xy_idx(0, y)] = TileType::Wall;
-        map[xy_idx(79, y)] = TileType::Wall;
+impl Map {
+    pub fn xy_idx(&self, x: i32, y: i32) -> usize {
+        (y as usize * self.width as usize) + x as usize
     }
 
-    let mut rng = rltk::RandomNumberGenerator::new();
-
-    // ランダムに壁を作る
-    for _i in 0..400 {
-        let x = rng.roll_dice(1, 79);
-        let y = rng.roll_dice(1, 49);
-        let idx = xy_idx(x, y);
-
-        // mapの中央は壁にしたくない.ここから始めたい
-        if idx != xy_idx(40, 25) {
-            map[idx] = TileType::Wall;
-        }
-    }
-
-    map
-}
-
-// 部屋を作る. 与えられたrectのタイルをすべて床にする
-fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
-    for y in room.y1 + 1..=room.y2 {
-        for x in room.x1 + 1..=room.x2 {
-            map[xy_idx(x, y)] = TileType::Floor;
-        }
-    }
-}
-
-fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32) {
-    for x in min(x1, x2)..=max(x1, x2) {
-        let idx = xy_idx(x, y);
-        if idx > 0 && idx < 80 * 50 {
-            map[idx as usize] = TileType::Floor;
-        }
-    }
-}
-
-fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
-    for y in min(y1, y2)..=max(y1, y2) {
-        let idx = xy_idx(x, y);
-        if idx > 0 && idx < 80 * 50 {
-            map[idx as usize] = TileType::Floor;
-        }
-    }
-}
-
-pub fn new_map_rooms_and_corridors() -> (Vec<Rect>, Vec<TileType>) {
-    let mut map = vec![TileType::Wall; 80 * 50];
-
-    // ↓で作ったお部屋たちを登録してく
-    let mut rooms: Vec<Rect> = Vec::new();
-    const MAX_ROOMS: i32 = 30;
-    const MIN_SIZE: i32 = 6;
-    const MAX_SIZE: i32 = 10;
-
-    let mut rng = RandomNumberGenerator::new();
-
-    for _ in 0..MAX_ROOMS {
-        let w = rng.range(MIN_SIZE, MAX_SIZE);
-        let h = rng.range(MIN_SIZE, MAX_SIZE);
-        let x = rng.roll_dice(1, 80 - w - 1) - 1;
-        let y = rng.roll_dice(1, 50 - h - 1) - 1;
-        let new_room = Rect::new(x, y, w, h);
-        let mut ok = true;
-
-        // 作ろうとしてる部屋が既存の部屋と重なってたら作るのやめる
-        for other_room in rooms.iter() {
-            if new_room.intersect(other_room) {
-                ok = false
+    // 部屋を作る. 与えられたrectのタイルをすべて床にする
+    fn apply_room_to_map(&mut self, room: &Rect) {
+        for y in room.y1 + 1..=room.y2 {
+            for x in room.x1 + 1..=room.x2 {
+                let idx = self.xy_idx(x, y);
+                self.tiles[idx] = TileType::Floor;
             }
         }
-        if ok {
-            apply_room_to_map(&new_room, &mut map);
+    }
 
-            // 部屋を廊下でつなげる
-            if !rooms.is_empty() {
-                // その部屋の真ん中の座標を得る
-                let (new_x, new_y) = new_room.center();
-                // 1つ前の部屋の真ん中の座標を得る
-                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+        for x in min(x1, x2)..=max(x1, x2) {
+            let idx = self.xy_idx(x, y);
+            if idx > 0 && idx < self.width as usize * self.height as usize {
+                self.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
 
-                // 0か1のサイコロふる
-                if rng.range(0, 2) == 1 {
-                    apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y);
-                    apply_vertical_tunnel(&mut map, prev_y, new_y, new_x);
-                } else {
-                    apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x);
-                    apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y);
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+        for y in min(y1, y2)..=max(y1, y2) {
+            let idx = self.xy_idx(x, y);
+            if idx > 0 && idx < self.width as usize * self.height as usize {
+                self.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    /// Makes a new map using the algorithm from http://rogueliketutorials.com/tutorials/tcod/part-3/
+    /// This gives a handful of random rooms and corridors joining them together.
+    pub fn new_map_rooms_and_corridors() -> Map {
+        let mut map = Map {
+            tiles: vec![TileType::Wall; 80 * 50],
+            rooms: Vec::new(),
+            width: 80,
+            height: 50,
+        };
+
+        // ↓で作ったお部屋たちを登録してく
+        const MAX_ROOMS: i32 = 30;
+        const MIN_SIZE: i32 = 6;
+        const MAX_SIZE: i32 = 10;
+
+        let mut rng = RandomNumberGenerator::new();
+
+        for _ in 0..MAX_ROOMS {
+            let w = rng.range(MIN_SIZE, MAX_SIZE);
+            let h = rng.range(MIN_SIZE, MAX_SIZE);
+            let x = rng.roll_dice(1, 80 - w - 1) - 1;
+            let y = rng.roll_dice(1, 50 - h - 1) - 1;
+            let new_room = Rect::new(x, y, w, h);
+            let mut ok = true;
+
+            // 作ろうとしてる部屋が既存の部屋と重なってたら作るのやめる
+            for other_room in map.rooms.iter() {
+                if new_room.intersect(other_room) {
+                    ok = false
                 }
             }
+            if ok {
+                map.apply_room_to_map(&new_room);
 
-            rooms.push(new_room);
+                // 部屋を廊下でつなげる
+                if !map.rooms.is_empty() {
+                    // その部屋の真ん中の座標を得る
+                    let (new_x, new_y) = new_room.center();
+                    // 1つ前の部屋の真ん中の座標を得る
+                    let (prev_x, prev_y) = map.rooms[map.rooms.len() - 1].center();
+
+                    // 0か1のサイコロふる
+                    if rng.range(0, 2) == 1 {
+                        map.apply_horizontal_tunnel(prev_x, new_x, prev_y);
+                        map.apply_vertical_tunnel(prev_y, new_y, new_x);
+                    } else {
+                        map.apply_vertical_tunnel(prev_y, new_y, prev_x);
+                        map.apply_horizontal_tunnel(prev_x, new_x, new_y);
+                    }
+                }
+
+                map.rooms.push(new_room);
+            }
         }
-    }
 
-    (rooms, map)
+        map
+    }
 }
 
 // TileTypeのスライスを足す
