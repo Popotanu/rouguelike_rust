813904e08c3593cdb15d6e5c0106801d9113ea40
popotanu <scrapmacaron@gmail.com>
2023-01-25 11:49:42 +0900

    feat(`SufferDamage`,`WantsToMelee`): 負うダメージを`SufferDamage`に追加する/攻撃対象が存在すれば攻撃意思があるとみなす

--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,4 @@
-use crate::{CombatStats, Viewshed};
+use crate::{CombatStats, Viewshed, WantsToMelee};
 
 use super::{Map, Player, Point, Position, RunState, State, TileType};
 use rltk::{console, Rltk, VirtualKeyCode};
@@ -12,18 +12,36 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let combat_stats = ecs.read_storage::<CombatStats>();
     let map = ecs.fetch::<Map>();
 
-    for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
+    let entities = ecs.entities();
+    let mut wants_to_melee = ecs.write_storage::<WantsToMelee>();
+
+    for (entity, _player, pos, viewshed) in
+        (&entities, &mut players, &mut positions, &mut viewsheds).join()
+    {
+        if pos.x + delta_x < 1
+            || pos.x + delta_x > map.width - 1
+            || pos.y + delta_y < 1
+            || pos.y + delta_y > map.height - 1
+        {
+            return;
+        }
+
         let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
 
         for potential_target in map.tile_content[destination_idx].iter() {
             let target = combat_stats.get(*potential_target);
-            match target {
-                None => {}
-                Some(t) => {
-                    // こうげき
-                    console::log(&format!("From Hell's Heart, I stab thee!"));
-                    return; // 攻撃したあとは動かない
-                }
+
+            // 攻撃対象が存在すれば攻撃意思があるとみなす
+            if let Some(_target) = target {
+                wants_to_melee
+                    .insert(
+                        entity,
+                        WantsToMelee {
+                            target: *potential_target,
+                        },
+                    )
+                    .expect("Add target failed");
+                return;
             }
         }
 
