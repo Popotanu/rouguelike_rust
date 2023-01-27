780f553dd58c14a4bb65ef166fd8e921a9447145
popotanu <scrapmacaron@gmail.com>
2023-01-27 21:57:12 +0900

    refactor(spawner): playerとmonsterの出現ロジックを`Spawner`に移動する

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ use damage_system::DamageSystem;
 mod gui;
 use gui::*;
 mod gamelog;
-
+mod spawner;
 
 // 待ち状態(相手のターン) or 自分のターン
 #[derive(PartialEq, Copy, Clone, Debug)]
@@ -130,90 +130,22 @@ fn main() -> rltk::BError {
     gs.ecs.insert(gamelog::GameLog {
         entries: vec!["Welcome to Rusty Roguelike".to_string()],
     });
-    gs.ecs.insert(rltk::RandomNumberGenerator::new());
 
     let map: Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
 
-    // Mobをつくる
-    let mut rng = rltk::RandomNumberGenerator::new();
-    // skip(1) で最初の部屋にはmob配置しないようにする
-    // playerが配置されるから
-    for (i, room) in map.rooms.iter().skip(1).enumerate() {
-        let (x, y) = room.center();
-
-        let glyph: rltk::FontCharType;
-        let name: String;
-        let roll = rng.roll_dice(1, 2);
-        match roll {
-            1 => {
-                glyph = rltk::to_cp437('g');
-                name = "Goblin".to_string();
-            }
-            _ => {
-                glyph = rltk::to_cp437('o');
-                name = "Orc".to_string();
-            }
-        }
-
-        gs.ecs
-            .create_entity()
-            .with(Position { x, y })
-            .with(Renderable {
-                glyph: glyph,
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(Viewshed {
-                visible_tiles: Vec::new(),
-                range: 8,
-                dirty: true,
-            })
-            .with(Monster {})
-            .with(Name {
-                name: format!("{} #{}", &name, i),
-            })
-            .with(BlocksTile {})
-            .with(CombatStats {
-                max_hp: 16,
-                hp: 16,
-                defense: 1,
-                power: 4,
-            })
-            .build();
-    }
-
     // playerをつくる
     // 初期位置を部屋の真ん中にする
-    let player_entity = gs
-        .ecs
-        .create_entity()
-        .with(Position {
-            x: player_x,
-            y: player_y,
-        })
-        .with(Renderable {
-            glyph: rltk::to_cp437('@'),
-            fg: RGB::named(rltk::YELLOW),
-            bg: RGB::named(rltk::BLACK),
-        })
-        .with(Player {})
-        .with(Viewshed {
-            visible_tiles: Vec::new(),
-            range: 8,
-            dirty: true,
-        })
-        .with(Name {
-            name: "Pon".to_string(),
-        })
-        .with(CombatStats {
-            max_hp: 30,
-            hp: 30,
-            defense: 2,
-            power: 5,
-        })
-        .build();
+    let player_entity = spawner::player(&mut gs.ecs, player_x, player_y);
 
+    gs.ecs.insert(rltk::RandomNumberGenerator::new());
+    // Mobをつくる
+    // skip(1) で最初の部屋にはmob配置しないようにする
+    // playerが配置されるから
+    for room in map.rooms.iter().skip(1) {
+        let (x, y) = room.center();
+        spawner::random_monster(&mut gs.ecs, x, y);
+    }
     gs.ecs.insert(map);
     // ecsのsystemにplayerの居場所を伝える
     gs.ecs.insert(Point::new(player_x, player_y));
