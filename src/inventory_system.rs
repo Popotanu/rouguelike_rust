7ebb16f5cb45b75bc08f3bcf40668157d96477ae
popotanu <scrapmacaron@gmail.com>
2023-02-04 22:07:40 +0900

    feat: 混乱対象を登録しログを流す

--- src/inventory_system.rs
+++ src/inventory_system.rs
@@ -1,6 +1,7 @@
 use super::{
-    gamelog::GameLog, AreaOfEffect, CombatStats, Consumable, InBackpack, InflictsDamage, Map, Name,
-    Position, ProvidesHealing, SufferDamage, WantsToDropItem, WantsToPickupItem, WantsToUseItem,
+    gamelog::GameLog, AreaOfEffect, CombatStats, Confusion, Consumable, InBackpack, InflictsDamage,
+    Map, Name, Position, ProvidesHealing, SufferDamage, WantsToDropItem, WantsToPickupItem,
+    WantsToUseItem,
 };
 use specs::prelude::*;
 
@@ -58,6 +59,7 @@ impl<'a> System<'a> for ItemUseSystem {
         ReadStorage<'a, InflictsDamage>,
         ReadStorage<'a, Consumable>,
         ReadStorage<'a, AreaOfEffect>,
+        WriteStorage<'a, Confusion>,
         WriteStorage<'a, CombatStats>,
         WriteStorage<'a, SufferDamage>,
     );
@@ -74,6 +76,7 @@ impl<'a> System<'a> for ItemUseSystem {
             inflict_damage,
             consumables,
             aoe,
+            mut confused,
             mut combat_stats,
             mut suffer_damage,
         ) = data;
@@ -157,6 +160,35 @@ impl<'a> System<'a> for ItemUseSystem {
                         used_item = true;
                     }
                 }
+            }
+
+            // こんらん
+            let mut add_confusion = Vec::new();
+            // 借用チェッカーみたすための{}
+            {
+                let causes_confusion = confused.get(useitem.item);
+                match causes_confusion {
+                    None => {}
+                    Some(confusion) => {
+                        used_item = false;
+                        for mob in targets.iter() {
+                            add_confusion.push((*mob, confusion.turns));
+                            if entity == *player_entity {
+                                let mob_name = names.get(*mob).unwrap();
+                                let item_name = names.get(useitem.item).unwrap();
+                                gamelog.entries.push(format!(
+                                    "You use {} on {}, confusing them.",
+                                    item_name.name, mob_name.name
+                                ));
+                            }
+                        }
+                    }
+                }
+            }
+            for mob in add_confusion.iter() {
+                confused
+                    .insert(mob.0, Confusion { turns: mob.1 })
+                    .expect("Unable to insert status");
             }
 
             if used_item {
